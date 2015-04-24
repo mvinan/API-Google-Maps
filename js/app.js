39e5b08feb908c4970cd982384bc20d0687d5109
Miguel Alejandro <mvinan@icloud.com>
2015-04-23 23:58:32 -0500

    Creando mapa

--- js/app.js
+++ js/app.js
@@ -1,16 +1,3 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
-
-	var map;
-
-      function initialize() {
-        var mapOptions = {
-          zoom: 8,
-          center: new google.maps.LatLng(-34.397, 150.644)
-        };
-        map = new google.maps.Map(document.getElementById('map-canvas'),
-            mapOptions);
-      }
-
-      google.maps.event.addDomListener(window, 'load', initialize);
