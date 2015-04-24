2b7b4ecd53f26f45fe0e76d564bbfd66aa569698
Miguel Alejandro <mvinan@icloud.com>
2015-04-24 00:15:47 -0500

    WTF

--- js/js.js
+++ js/js.js
@@ -1,10 +1,5 @@
 (function (){
-
-  // var API_MAPS_KEY = "AIzaSyA7nSWoLc22iPe3Gs-O8BAqs9TZs4RVOJU";
-  // var API_MAPS_URL = "https://maps.googleapis.com/maps/api/js"+"?"+API_MAPS_KEY;
-
-  // $.getJSON(API_MAPS_URL);
-
+  
 	var map;
 
       function initialize() {
