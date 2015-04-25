e857c3f5f82ffc57ba0a4241b4ed7030cd209921
Miguel Alejandro <mvinan@icloud.com>
2015-04-24 23:31:10 -0500

    depurado de archivos y agregando estilos al tooltip

--- js/googleMaps.js
+++ js/googleMaps.js
@@ -44,6 +44,32 @@
             }
           ];
 
+          var contentString = 
+          '<div id="content">'+
+                '<div id="siteNotice">'+
+                '</div>'+
+                '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
+                '<div id="bodyContent">'+
+                '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+                'sandstone rock formation in the southern part of the '+
+                'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
+                'south west of the nearest large town, Alice Springs; 450&#160;km '+
+                '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
+                'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+                'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+                'Aboriginal people of the area. It has many springs, waterholes, '+
+                'rock caves and ancient paintings. Uluru is listed as a World '+
+                'Heritage Site.</p>'+
+                '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+                'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+                '(last visited June 22, 2009).</p>'+
+                '</div>'+
+                '</div>';
+
+        var infowindow = new google.maps.InfoWindow({
+              content: contentString
+          });
+
         // Creamos un nuevo objeto StyleMapType, pasando los estilos del array,
         // asi como el nombre que se mostrara en el control de tipo de mapa
         var styledMap = new google.maps.StyledMapType(styles,
@@ -81,19 +107,20 @@
         *********************************************************************/
 
         // Dibujando un  Marcador Personalizado.
-        /*var svgDibujado = {
-          path: 'M 125,5 155,90 245,90 175,145 200,230 125,180 50,230 75,145 5,90 95,90 z',
-          fillColor: "yellow",
-          fillOpacity: 0.8,
-          scale: 0.1,
-          strokeColor: "gold",
+        var svgDibujado = {
+          path: 'M0.05-20.53c-3.89,0-7.05,3.16-7.05,7.05C-7-9.59,0.05-1,0.05-1
+    S7.1-9.59,7.1-13.48C7.1-17.38,3.94-20.53,0.05-20.53z M0.05-11.28c-1.74,0-3.15-1.41-3.15-3.16c0-1.74,1.41-3.15,3.15-3.15c1.74,0,3.16,1.41,3.16,3.15C3.2-12.69,1.79-11.28,0.05-11.28z',
+          fillColor: "#D4145A",
+          fillOpacity: 1,
+          scale: 3,
+          strokeColor: "white",
           strokeWeight: 2
-        }; */
+        };
 
         // Creando el marcador para el mapa
         var marker = new google.maps.Marker({
             position: ubicacion_sitio,
-            // icon: svgDibujado, /* aqui va el nombre de la variable del dibujo*/
+            icon: svgDibujado, /* aqui va el nombre de la variable del dibujo*/
             title:"Marker Title",
             draggable:true,
             animation: google.maps.Animation.BOUNCE,/*Se agrega la animacion aqui  DROP que cae al momento de crear el maker , BOUNCE que debe rebotar*/
@@ -104,20 +131,35 @@
         map.mapTypes.set('map_style', styledMap);
         map.setMapTypeId('map_style');
 
-        // llamamos a la marka
+        // llamamos a la marca
         marker.setMap(map);
 
+        // llamamos al tooltip
+        google.maps.event.addListener(marker, 'click', function() {
+          infowindow.open(map,marker);
+         });
+
       }
 
-      function toggleBounce() {
+function toggleBounce() 
+{
 
-        if (marker.getAnimation() != null) {
-          marker.setAnimation(null);
-        } else {
-          marker.setAnimation(google.maps.Animation.BOUNCE);
-        }
-      }
+    if (marker.getAnimation() != null) 
+    {
+        marker.setAnimation(null);
+    } 
+    else 
+    {
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+    }
 
+}
 
-      google.maps.event.addDomListener(window, 'load', initialize);
+/********************************************************
+*                    EVENTOS                            *
+*********************************************************/
+      
+// Llamamos al mapa
+google.maps.event.addDomListener(window, 'load', initialize);
+
 })();
